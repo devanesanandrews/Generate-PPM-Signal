de02463135eb72ddf8de29a3727eae661fd5742e
devanesanandrews <devanesanandrews@gmail.com>
2023-12-02 19:55:28 +0530

    Update Generate-PPM-Signal.ino

--- Generate-PPM-Signal.ino
+++ Generate-PPM-Signal.ino
@@ -1,23 +1,25 @@
-//this programm will put out a PPM signal
+/*
+ * PPM generator originally written by David Hasko
+ * on https://code.google.com/p/generate-ppm-signal/ 
+ */
 
 //////////////////////CONFIGURATION///////////////////////////////
-#define chanel_number 8  //set the number of chanels
-#define default_servo_value 1500  //set the default servo value
-#define PPM_FrLen 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
-#define PPM_PulseLen 300  //set the pulse length
+#define CHANNEL_NUMBER 12  //set the number of chanels
+#define CHANNEL_DEFAULT_VALUE 1500  //set the default servo value
+#define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 300  //set the pulse length
 #define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
 #define sigPin 10  //set PPM signal output pin on the arduino
-//////////////////////////////////////////////////////////////////
-
 
 /*this array holds the servo values for the ppm signal
  change theese values in your code (usually servo values move between 1000 and 2000)*/
-int ppm[chanel_number];
+int ppm[CHANNEL_NUMBER];
 
 void setup(){  
+
   //initiallize default ppm values
-  for(int i=0; i<chanel_number; i++){
-    ppm[i]= default_servo_value;
+  for(int i=0; i<CHANNEL_NUMBER; i++){
+      ppm[i]= CHANNEL_DEFAULT_VALUE;
   }
 
   pinMode(sigPin, OUTPUT);
@@ -32,16 +34,17 @@ void setup(){
   TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
   TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
   sei();
+
 }
 
 void loop(){
-  //put main code here
-  static int val = 1;
   
-  ppm[0] = ppm[0] + val;
-  if(ppm[0] >= 2000){ val = -1; }
-  if(ppm[0] <= 1000){ val = 1; }
-  delay(10);
+  /*
+    Here modify ppm array and set any channel to value between 1000 and 2000. 
+    Timer running in the background will take care of the rest and automatically 
+    generate PPM signal on output pin using values in ppm array
+  */
+  
 }
 
 ISR(TIMER1_COMPA_vect){  //leave this alone
@@ -49,26 +52,25 @@ ISR(TIMER1_COMPA_vect){  //leave this alone
   
   TCNT1 = 0;
   
-  if(state) {  //start pulse
+  if (state) {  //start pulse
     digitalWrite(sigPin, onState);
-    OCR1A = PPM_PulseLen * 2;
+    OCR1A = PULSE_LENGTH * 2;
     state = false;
-  }
-  else{  //end pulse and calculate when to start the next pulse
+  } else{  //end pulse and calculate when to start the next pulse
     static byte cur_chan_numb;
     static unsigned int calc_rest;
   
     digitalWrite(sigPin, !onState);
     state = true;
 
-    if(cur_chan_numb >= chanel_number){
+    if(cur_chan_numb >= CHANNEL_NUMBER){
       cur_chan_numb = 0;
-      calc_rest = calc_rest + PPM_PulseLen;// 
-      OCR1A = (PPM_FrLen - calc_rest) * 2;
+      calc_rest = calc_rest + PULSE_LENGTH;// 
+      OCR1A = (FRAME_LENGTH - calc_rest) * 2;
       calc_rest = 0;
     }
     else{
-      OCR1A = (ppm[cur_chan_numb] - PPM_PulseLen) * 2;
+      OCR1A = (ppm[cur_chan_numb] - PULSE_LENGTH) * 2;
       calc_rest = calc_rest + ppm[cur_chan_numb];
       cur_chan_numb++;
     }     
